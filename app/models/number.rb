2670ec1bc67b0f87954eb9c1a2fd95d5f9426707
Jason Sommerset <jasommerset@gmail.com>
2015-05-06 13:13:57 -0700

    init

--- app/models/number.rb
+++ app/models/number.rb
@@ -2,6 +2,6 @@ class Number < ActiveRecord::Base
 
 validates :area_code, :presence => true
 validates :business_number, :presence => true
-validates :tracking_number, :presence => { message: "No tracking numbers available for that area code. Try a new area code." }
+validates :tracking_number, :presence => { message: "not available for that area code. Try a new area code." }
 
 end
